ae2d38a3fe9085c60cdcde10f666f7a4c834c937
khafid-gionino <achmadkhafids75@gmail.com>
2020-03-31 11:55:06 +0700

    commit

---
+++ GanjilGenap/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtangka = New System.Windows.Forms.TextBox()
+        Me.proses = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Ubuntu Mono", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(58, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(252, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "APPLIKASI GANJIL GENAP"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Masukkan Angka :"
+        '
+        'txtangka
+        '
+        Me.txtangka.Location = New System.Drawing.Point(125, 63)
+        Me.txtangka.Name = "txtangka"
+        Me.txtangka.Size = New System.Drawing.Size(210, 20)
+        Me.txtangka.TabIndex = 2
+        '
+        'proses
+        '
+        Me.proses.Location = New System.Drawing.Point(260, 98)
+        Me.proses.Name = "proses"
+        Me.proses.Size = New System.Drawing.Size(75, 23)
+        Me.proses.TabIndex = 3
+        Me.proses.Text = "Proses"
+        Me.proses.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Viner Hand ITC", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 183)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(122, 21)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Ach.Khafid Salim"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(363, 213)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.proses)
+        Me.Controls.Add(Me.txtangka)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtangka As TextBox
+    Friend WithEvents proses As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ GanjilGenap/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Private Sub proses_Click(sender As Object, e As EventArgs) Handles proses.Click
+        'Try..Catch..Finally..End Try
+        Try
+            Dim X As Integer
+            X = txtangka.Text
+            Do
+                X = X Mod 2
+                If X = 0 Then
+                    MessageBox.Show("Bilangan Genap", "Informasi")
+                    Exit Do
+                ElseIf X = 1 Then
+                    MessageBox.Show("Bilangan Ganjil", "Informasi")
+                    Exit Do
+                End If
+
+            Loop While Not (X)
+        Catch ex As Exception
+            MessageBox.Show("Inputan Harus Berupa Angka !!", "Informasi")
+        Finally
+            txtangka.Text = ""
+            txtangka.Focus()
+        End Try
+
+    End Sub
+
+
+
+    'on error resume next
+    On Error Resume Next
+
+    Dim X As Integer
+        X = txtangka.Text
+        If X = 2 Then
+            MessageBox.Show("Bilangan Genap", "Informasi")
+        ElseIf X = 1 Then
+            MessageBox.Show("Bilangan Ganjil", "Informasi")
+        End If
+
+
+
+    'On Error Goto
+    On Error GoTo informasi
+
+        Dim X As Integer
+        X = txtangka.Text
+        If X = 2 Then
+            MessageBox.Show("Bilangan Genap", "Informasi")
+        ElseIf X = 1 Then
+            MessageBox.Show("Bilangan Ganjil", "Informasi")
+        End If
+
+informasi:
+        MessageBox.Show("Inputan tidak boleh kosong !!", "Error")
+    End Sub
+End Class
